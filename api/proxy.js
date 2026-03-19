b7f7670f1b30ff1e2ff247939cac9d6c3e604d6f
Cyesil <cyesil@gmail.com>
2026-03-19 17:15:22 +0100

    Update api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,22 +5,23 @@ module.exports = (req, res) => {
   const url = `https://forening-api.svenskfotboll.se/club${path}`;
   const API_KEY = '22a66c836d2f49a3bb4820131eb5d1a4';
 
-  https.get(url, {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+
+  const request = https.get(url, {
     headers: {
       'ApiKey': API_KEY,
       'Accept': 'application/json',
     }
-  }, (apiRes) => {
+  }, (response) => {
     let data = '';
-    apiRes.on('data', chunk => data += chunk);
-    apiRes.on('end', () => {
-      res.setHeader('Content-Type', 'application/json');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-      res.status(apiRes.statusCode).send(data);
+    response.on('data', chunk => data += chunk);
+    response.on('end', () => {
+      res.status(response.statusCode).send(data);
     });
-  }).on('error', (err) => {
+  });
+
+  request.on('error', (err) => {
     res.status(500).json({ error: err.message });
   });
 };
